360997c2d039fe5c67ddc16ca0a8df8faf6a6d49
maliayyildiz <maliayyildiz61@gmail.com>
2021-07-31 15:09:22 +0200

    haedless chrome

--- src/test/java/tests/GithubActionTest.java
+++ src/test/java/tests/GithubActionTest.java
@@ -3,13 +3,14 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class GithubActionTest {
     @Test
     public void test(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
 
         driver.get("https://www.google.com/");
 
